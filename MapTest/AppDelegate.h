b20361a79fa99f32124dae2c565645a9a4cbdbeb
butterfly <675376640@qq.com>
2016-10-18 13:12:10 +0800

    Initial Commit

---
+++ MapTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MapTest
+//
+//  Created by xhm on 16/10/18.
+//  Copyright © 2016年 xhm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MapTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MapTest
+//
+//  Created by xhm on 16/10/18.
+//  Copyright © 2016年 xhm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
